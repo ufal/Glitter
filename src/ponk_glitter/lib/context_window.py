4e99fefcea3842b663d97c98ad3b9ed04797d7ce
Jan Černý <jc@ucw.cz>
2024-04-11 16:00:18 +0200

    merge context widows to one file

--- src/ponk_glitter/lib/context_window.py
+++ src/ponk_glitter/lib/context_window.py
@@ -34,3 +34,26 @@ class ContextWindow:
 
     def __repr__(self):
         return repr(self.text)
+
+
+class MaskedContextWindow(ContextWindow):
+
+    def __init__(self, text: str, size: int, mask: str = "[MASK]"):
+        super().__init__(text, size)
+        self.mask = mask
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        window = super().__next__()
+        return " ".join(window) + " " + self.mask
+
+    def __len__(self):
+        return super().__len__()
+
+    def __getitem__(self, index):
+        return super().__get_window__(index)
+
+    def __repr__(self):
+        return repr(self.mask)

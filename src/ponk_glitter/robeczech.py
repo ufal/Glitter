0001118aa6468918b87e17b3296d883ce0c5a178
Jan Černý <jc@ucw.cz>
2024-04-11 12:12:48 +0200

    robeczech: remake progress tracking

--- src/ponk_glitter/robeczech.py
+++ src/ponk_glitter/robeczech.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+from random import choice
 
 from rich.progress import track
 from transformers import AutoTokenizer, AutoModelForMaskedLM, pipeline, logging, AutoModelForCausalLM
+
+from lib.glitter_common import GlitterModel
 from lib.glitter_common import GlitteredToken, GlitteredText
 from lib.masked_context_window import MaskedContextWindow
-from lib.glitter_common import GlitterModel
 
 logging.set_verbosity(logging.CRITICAL)
 
 
 class Robeczech(GlitterModel):
+    SPECIAL_TOKENS = ["[SEP]", "[CLS]"]
 
     def __init__(self,
                  context_window_size: int = 5,
@@ -17,9 +20,9 @@ class Robeczech(GlitterModel):
         super().__init__("Robeczech", "cs", context_window_size=context_window_size, sample_size=top_k)
         self.context_window_size = context_window_size
         self.top_k = top_k
-
         self.model = AutoModelForCausalLM.from_pretrained("models/robeczech")
         self.tokenizer = AutoTokenizer.from_pretrained("models/robeczech")
+        self.tokenizer.add_special_tokens({"additional_special_tokens": self.SPECIAL_TOKENS})
         self.model = AutoModelForMaskedLM.from_pretrained("models/robeczech")
         self.pipe = pipeline('fill-mask', model=self.model, tokenizer=self.tokenizer)
 
@@ -33,14 +36,18 @@ class Robeczech(GlitterModel):
     def glitter_text(self, text: str) -> GlitteredText:
         gt = GlitteredText(models=["Robeczech"])
         split_text = text.split()
-        for ot, mcw, _ in zip(split_text,
-                              MaskedContextWindow(text, self.context_window_size),
-                              track(range(len(split_text) + 1), description="Glittering...")):
+        for ot, mcw in track(zip(split_text,
+                                    MaskedContextWindow(text, self.context_window_size)),
+                                description="Glittering...",
+                                total=len(split_text)):
             gt.append(self.glitter_masked_token(ot, mcw))
         return gt
 
-    def generate_text(self, prompt, length=20, top_k=50):
+    def generate_text(self, prompt: str, length: int = 20, top_k: int = 50) -> str:
         text = prompt
-        for i in range(length):
-            text += " " + self.pipe(text + " [MASK]", top_k=top_k)
+        for i in track(range(length), description="Generating...", total=length):
+            new_token = choice(self.pipe(text + " [MASK]", top_k=top_k))["token_str"]
+            text += " " if new_token == "[SEP]" else new_token
+        for st in self.SPECIAL_TOKENS:
+            text = text.replace(st, "")
         return text

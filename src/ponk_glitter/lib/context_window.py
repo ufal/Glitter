be9f69478567d11e08a0df377e8abd435e861a1b
Jan Černý <jc@ucw.cz>
2024-04-11 23:20:36 +0200

    reimplement context window to support tokenized text

--- src/ponk_glitter/lib/context_window.py
+++ src/ponk_glitter/lib/context_window.py
@@ -5,41 +5,41 @@ class ContextWindow:
     and it grows to defined size.
     """
 
-    def __init__(self, text: str, size: int):
-        self.text = text.strip().split()
+    def __init__(self, tokenized_text: [str], size: int):
+        self.tokenized_text = tokenized_text
         self.size = size
         self.index = 0
 
     def __get_window__(self, index: int):
         if self.index < self.size:
-            return self.text[0: index]
+            return self.tokenized_text[0: index]
         else:
-            return self.text[self.index - self.size: index]
+            return self.tokenized_text[self.index - self.size: index]
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.index >= len(self.text):
+        if self.index >= len(self.tokenized_text):
             raise StopIteration
         window = self.__get_window__(self.index)
         self.index += 1
         return window
 
     def __len__(self):
-        return len(self.text) - 1  # nejsem si jistej tou -1
+        return len(self.tokenized_text) - 1  # nejsem si jistej tou -1
 
     def __getitem__(self, index):
         return self.__get_window__(index)
 
     def __repr__(self):
-        return repr(self.text)
+        return repr(self.tokenized_text)
 
 
 class MaskedContextWindow(ContextWindow):
 
-    def __init__(self, text: str, size: int, mask: str = "[MASK]"):
-        super().__init__(text, size)
+    def __init__(self, tokenized_text: str, size: int, mask: str = "[MASK]"):
+        super().__init__(tokenized_text, size)
         self.mask = mask
 
     def __iter__(self):
@@ -47,7 +47,7 @@ class MaskedContextWindow(ContextWindow):
 
     def __next__(self):
         window = super().__next__()
-        return " ".join(window) + " " + self.mask
+        return window + [self.mask]
 
     def __len__(self):
         return super().__len__()

5fd15d08252e406f7acc4e5667f0f497d1504008
Jan Černý <jc@ucw.cz>
2024-05-15 21:36:28 +0200

    rename address argument to host

--- src/ponk_glitter/lib/arguments.py
+++ src/ponk_glitter/lib/arguments.py
@@ -9,7 +9,7 @@ def get_args():
                         default=False,
                         action="store_true",
                         help="Run server in debug mode")
-    parser.add_argument("--address",
+    parser.add_argument("--host",
                         default="127.0.0.1",
                         help="Address to bind server to (0.0.0.0 for nonlocal use)")
     parser.add_argument("--port",

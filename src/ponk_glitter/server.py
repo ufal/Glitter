d27ac4a3d8555e4f5c0d9ea15347dfd640cacdc0
Jan Černý <jc@ucw.cz>
2024-05-20 15:01:02 +0200

    server.py: generalize template rendering + add caching

--- src/ponk_glitter/server.py
+++ src/ponk_glitter/server.py
@@ -6,6 +6,7 @@ from models.robeczech import Robeczech
 from lib.glitter_common import GlitteredText
 
 MODELS = dict()
+COLOR_MODES = ("simple", "heatmap")
 app = Flask(__name__, static_folder="static")
 
 
@@ -16,9 +17,16 @@ def sanitize_path(path):
     return path.replace("../", "").replace("./", "")
 
 
+def render_index_page(glittered_text="", models=MODELS.keys(), color_modes=COLOR_MODES):
+    return render_template("index.html",
+                           glittered_text=glittered_text,
+                           color_modes=color_modes,
+                           models=models)
+
+
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_index_page("", MODELS.keys())
 
 
 @app.route("/<path:path>", methods=["GET"])
@@ -27,15 +35,23 @@ def static_route(path):
     return send_from_directory("static", path)
 
 
+
 ######################################################################################
 # API
 
-@app.route("/glitter_text", methods=["POST"])
-def glitter_text():
-    text_to_glitter = request.form["text_to_glitter"]
-    model = MODELS[request.form["model"]]
+@cache
+def glitter_text(text_to_glitter: str, model_name: str, color_mode: str) -> str:
+    model = MODELS[model_name]
     glittered_text = model.glitter_text(text_to_glitter)
-    return render_template("index.html", glittered_text=glittered_text.to_html())
+    return glittered_text.to_html()
+
+
+@app.route("/", methods=["POST"])
+def glitter_text_request():
+    text_to_glitter = request.form["text_to_glitter"]
+    model_name = request.form["model"]
+    #color_mode = request.form["color_mode"]
+    return render_index_page(glitter_text(text_to_glitter, model_name, ""))
 
 
 ######################################################################################
@@ -43,11 +59,11 @@ def glitter_text():
 
 def server_init():
     global MODELS
-    MODELS["robeczech"] = Robeczech()
+    MODELS["Robeczech"] = Robeczech()
 
-    print("Models loaded:")
+    print(" * Models loaded:")
     for model_name in MODELS.keys():
-        print(f" - {model_name}")
+        print(f"   - {model_name}")
 
 
 if __name__ == "__main__":

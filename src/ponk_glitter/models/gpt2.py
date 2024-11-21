a7990b2b92aaa1b7f3df33a47c80666a4cd1c235
Jan Černý <jc@ucw.cz>
2024-11-21 14:23:50 +0100

    add GPT2 model to models

---
+++ src/ponk_glitter/models/gpt2.py
@@ -0,0 +1,14 @@
+from lib.glitter_models import GlitterGenerativeModel, register_model
+
+
+@register_model("gpt2")
+class Robeczech(GlitterGenerativeModel):
+
+    def __init__(self,
+                 context_window_size: int = 100,
+                 top_k=None):
+        super().__init__(name="GPT-2",
+                         lang="en",
+                         model_path="gpt2",
+                         context_window_size=context_window_size,
+                         top_k=top_k)

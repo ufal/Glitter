009232efb967440aca5ad74ee3e8e0abba340d50
Jan Černý <jc@ucw.cz>
2024-11-28 00:13:05 +0100

    add GPT2 medial czech

---
+++ src/ponk_glitter/models/gpt2-medical-czech.py
@@ -0,0 +1,12 @@
+from lib.glitter_models import GlitterGenerativeModel, register_model
+
+
+@register_model("gpt2-medical-czech")
+class Robeczech(GlitterGenerativeModel):
+
+    def __init__(self,
+                 context_window_size: int = 512):
+        super().__init__(name="GPT-2 medical Czech",
+                         lang="cs",
+                         model_path="/lchaloupsky/czech-gpt2-medical",
+                         context_window_size=context_window_size)

887414cd95cde30919c34ad0ec18529928f2001d
Jan Černý <jc@ucw.cz>
2024-12-10 00:31:48 +0100

    fix typo

--- src/ponk_glitter/models/gpt2-XL-czech.py
+++ src/ponk_glitter/models/gpt2-XL-czech.py
@@ -7,7 +7,7 @@ class Robeczech(GlitterGenerativeModel):
 
     def __init__(self,
                  context_window_size: int = 1024,
-                 top_k: Optinal[int] = 10):
+                 top_k: Optional[int] = 10):
         super().__init__(name="GPT-2 XL Czech",
                          lang="cs",
                          model_path="BUT-FIT/Czech-GPT-2-XL-133k",

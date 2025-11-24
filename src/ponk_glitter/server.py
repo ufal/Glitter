48e63b74d3a7f5123dfcb718791cbb13abf2dde1
Jan Černý <jc@ucw.cz>
2025-11-24 23:38:41 +0100

    DEV: update server conllu API logic

--- src/ponk_glitter/server.py
+++ src/ponk_glitter/server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from flask import Flask, send_from_directory, request, render_template
 import logging
 from functools import cache
-from conllu import parse
+
+import conllu
+from flask import Flask, send_from_directory, request, render_template
+
 from lib.arguments import get_server_args
 from lib.glitter_models import load_models, categorize_models
 
@@ -31,7 +33,6 @@ def render_index_page(text_to_glitter="",
         selected_model = tuple(models)[0]
     if selected_color_mode is None and color_modes:
         selected_color_mode = COLOR_MODES[0]
-
 
     return render_template("index.html",
                            text_to_glitter=text_to_glitter,
@@ -62,17 +63,19 @@ def glitter_text(text_to_glitter: str, model_name: str, color_mode: str) -> str:
     glittered_text = model.glitter_text(text_to_glitter, silent=SILENT_MODE)
     return glittered_text.to_html(color_mode=color_mode)
 
+
 @cache
-def glitter_text_to_conllu(conllu_string: str, model_name: str) -> str:
+def glitter_conllu_text(conllu_string: str, model_name: str) -> str:
     model = MODELS[model_name]
-    conllu_data = parse(conllu_string)
+    conllu_data = conllu.parse(conllu_string)
     text_to_glitter = ""
     for sentence in conllu_data:
         if "text" in sentence.metadata:
             for word in sentence:
-                text_to_glitter += f'{word["form".strip()] } '
+                text_to_glitter += f"{word["form".strip()]} "
     glittered_text = model.glitter_text(text_to_glitter, silent=SILENT_MODE)
     return glittered_text.to_conllu(conllu_data)
+
 
 @app.route("/", methods=["POST"])
 def glitter_text_request():
@@ -87,34 +90,37 @@ def glitter_text_request():
                              selected_model=model_name,
                              selected_color_mode=color_mode)
 
+
 @app.route('/process-conllu', methods=['POST'])
 def process_conllu():
-    conllu_string = request.data.decode('utf-8')
-    modified_conllu_string = glitter_text_to_conllu(conllu_string, "GPT-2 XL Czech") 
+    conllu_string = request.data.decode("utf-8")
+    modified_conllu_string = glitter_conllu_text(conllu_string, "GPT-2 XL Czech")
     result = {
-        'result': modified_conllu_string,
-        'colors' : 
+        "result": modified_conllu_string,
+        "colors":
             {
-                1 : "#6d6df7", 
-                2 : "#6d7ef7", 
-                3 : "#6d8ff7", 
-                4 : "#6da0f7", 
-                5 : "#6db2f7", 
-                6 : "#6dc3f7", 
-                7 : "#6dd4f7", 
-                8 : "#6de6f7", 
-                9 : "#6df7f7", 
-                10 : "#6df7b2", 
-                11 : "#6df76d", 
-                12 : "#b2f76d", 
-                13 : "#f7f76d", 
-                14 : "#f7d46d", 
-                15 : "#f7b26d", 
-                16 : "#f76d6d"
-            } 
-        }
+                1: "#6d6df7",
+                2: "#6d7ef7",
+                3: "#6d8ff7",
+                4: "#6da0f7",
+                5: "#6db2f7",
+                6: "#6dc3f7",
+                7: "#6dd4f7",
+                8: "#6de6f7",
+                9: "#6df7f7",
+                10: "#6df7b2",
+                11: "#6df76d",
+                12: "#b2f76d",
+                13: "#f7f76d",
+                14: "#f7d46d",
+                15: "#f7b26d",
+                16: "#f76d6d"
+            }
+    }
 
     return result
+
+
 ######################################################################################
 # Server
 
@@ -125,7 +131,6 @@ def detect_cuda():
         return torch.cuda.is_available()
     except ImportError:
         return False
-
 
 
 if __name__ == "__main__":
